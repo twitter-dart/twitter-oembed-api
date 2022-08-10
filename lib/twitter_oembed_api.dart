e3861df653a2805273f666577625a65753263394
myConsciousness <kato.shinya.dev@gmail.com>
2022-08-10 18:54:13 +0900

    add package

--- lib/twitter_oembed_api.dart
+++ lib/twitter_oembed_api.dart
@@ -9,3 +9,4 @@ export 'package:twitter_oembed_api/src/content_widget_type.dart';
 export 'package:twitter_oembed_api/src/embedded_timeline.dart';
 export 'package:twitter_oembed_api/src/embedded_tweet.dart';
 export 'package:twitter_oembed_api/src/twitter_oembed_api.dart';
+export 'package:twitter_oembed_api/src/twitter_oembed_exception.dart';
